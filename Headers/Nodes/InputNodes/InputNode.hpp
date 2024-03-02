3073509d0e4e3e99a04da52d0df83b6cfedad4f0
ZenMar2000 <enrico.marzola2000@gmail.com>
2024-03-02 16:29:37 +0100

    Fixed error in InputNode inheritance of virtual override functions

--- Headers/Nodes/InputNodes/InputNode.hpp
+++ Headers/Nodes/InputNodes/InputNode.hpp
@@ -2,20 +2,18 @@
 #define INPUTNODE_H
 
 #include "../Node.hpp"
-
 class InputNode : public Node
 {
 public:
 #pragma region "Constructors"
-    InputNode() : Node() {}
+    InputNode();
+    InputNode(NodeType type, Cell *parentCell);
 
-    InputNode(NodeType type, Cell *parentCell) : Node(type, parentCell){};
 #pragma endregion
 
 #pragma region "Functions"
-    void Activate() override;
-
-    int GenerateNodeGenome() override;
+    virtual void Activate() override;
+    virtual int GenerateNodeGenome() override;
 #pragma endregion
 };
 
--- Src/Nodes/InputNodes/InputNode.cpp
+++ Src/Nodes/InputNodes/InputNode.cpp
@@ -1,17 +1,25 @@
 #include "../../../Headers/Nodes/InputNodes/InputNode.hpp"
 #pragma region "Constructors"
 
-InputNode::InputNode() : Node() {}
+InputNode::InputNode(){}
 
-InputNode::InputNode(NodeType type, Cell *parentCell) : Node(type, parentCell)
+InputNode::InputNode(NodeType type, Cell *parentCell)
 {
-    if(type > 63){
+    if (type > 63)
+    {
         throw std::logic_error("Wrong NodeType used during generation. InputNode cannot accept NodeType: " + type);
     }
-   
 }
 #pragma endregion
 
 #pragma region "Functions"
+void InputNode::Activate()
+{
 
+}
+
+int InputNode::GenerateNodeGenome()
+{
+    return 0;
+}
 #pragma endregion
